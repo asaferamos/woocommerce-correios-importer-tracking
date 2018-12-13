6230fc4a7eb747b09e8aa4767dfff4d059e31030
Asafe Ramos <asafe@asaferamos.com>
2018-12-13 00:16:33 -0200

    remove test textarea

--- views/import.php
+++ views/import.php
@@ -3,8 +3,6 @@
 <hr>
 
 <form method="post">
-    <textarea style="width:100%;height:400px;" placeholder="Order Id    Tracking Code" name="tracking_codes">543	RE690245907BR
-944	RE690245898BR
-</textarea>
+    <textarea style="width:100%;height:400px;" placeholder="Order Id    Tracking Code" name="tracking_codes"></textarea>
     <input type="submit" name="wp-submit" class="button button-primary button-large" value="Import">
 </form>
